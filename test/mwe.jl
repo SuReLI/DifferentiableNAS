200bdaa7c1b32bf3cd8e6e287a364bb28849be27
k8lion <kaitlinmaile@gmail.com>
2020-12-10 23:09:54 +0100

    adjoint mwe

--- test/mwe.jl
+++ test/mwe.jl
@@ -7,8 +7,20 @@ function gshow(x)
 end
 Zygote.@adjoint function gshow(x)
     gshow(x), dx -> begin
-         @show typeof(dx) size(dx)
+         @show typeof(dx) size(dx) dx
          tuple(dx)
+    end
+end
+
+function my_softmax(xs; dims = 1)
+    #@show typeof(xs) xs
+    softmax(xs, dims = dims)
+end
+
+Zygote.@adjoint function my_softmax(xs; dims = 1)
+    softmax(xs, dims = dims), Δ -> begin
+        #@show typeof(Δ) Δ typeof(xs) xs ∇softmax(Δ, xs, dims = dims)
+        (∇softmax(Δ, xs, dims = dims),)
     end
 end
 
@@ -24,8 +36,9 @@ MixedOperation(channels::Int64, kernel_options::AbstractArray) =
     MixedOperation([ReLUConv(channels, channels, (i, i), i ÷ 2) for i in kernel_options])
 
 function (m::MixedOperation)(x::AbstractArray, αs::AbstractArray)
-    αs = gshow(softmax(gshow(αs)))
-    mapreduce((op, α) -> gshow(α * gshow(op(x))), +, m.operations, αs)
+    αs = my_softmax(αs)
+    sum(αs .* [op(x) for op in m.operations])
+    #mapreduce((op, α) -> α * op(x), +, m.operations, αs)
 end
 
 Flux.@functor MixedOperation
@@ -37,7 +50,7 @@ m = MixedOperation(3, [1, 3, 5, 7]) |> gpu
 αs = rand(Float32, 4) |> gpu
 test_image = rand(Float32, 16, 16, 3, 1) |> gpu
 @test sum(m(test_image, αs)) != 0
-grad = gradient((x,αs) -> gshow(sum(gshow(m(x,αs)))), test_image, αs)
+grad = gradient((x,αs) -> sum(m(x,αs)), test_image, αs)
 
 gαs = gradient(params(αs)) do
     sum(m(test_image, αs))

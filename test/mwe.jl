3cad1829b1a6cc7971a0b76191be220c9007c45d
k8lion <kaitlinmaile@gmail.com>
2020-12-10 23:05:02 +0100

    adjoint mwe

--- test/mwe.jl
+++ test/mwe.jl
@@ -2,15 +2,15 @@ using Flux
 using Zygote
 
 function gshow(x)
-         @show typeof(x) size(x)
-         x
-       end
+    @show typeof(x) size(x)
+    x
+end
 Zygote.@adjoint function gshow(x)
-       gshow(x), dx -> begin
+    gshow(x), dx -> begin
          @show typeof(dx) size(dx)
          tuple(dx)
-       end
-       end
+    end
+end
 
 ReLUConv(channels_in, channels_out, kernel_size, pad) =
     Chain(x -> relu.(x), Conv(kernel_size, channels_in => channels_out, pad = pad))
@@ -24,8 +24,8 @@ MixedOperation(channels::Int64, kernel_options::AbstractArray) =
     MixedOperation([ReLUConv(channels, channels, (i, i), i ÷ 2) for i in kernel_options])
 
 function (m::MixedOperation)(x::AbstractArray, αs::AbstractArray)
-    αs = softmax(gshow(αs))
-    mapreduce((op, α) -> α * op(x), +, m.operations, αs)
+    αs = gshow(softmax(gshow(αs)))
+    mapreduce((op, α) -> gshow(α * gshow(op(x))), +, m.operations, αs)
 end
 
 Flux.@functor MixedOperation
@@ -37,7 +37,7 @@ m = MixedOperation(3, [1, 3, 5, 7]) |> gpu
 αs = rand(Float32, 4) |> gpu
 test_image = rand(Float32, 16, 16, 3, 1) |> gpu
 @test sum(m(test_image, αs)) != 0
-grad = gradient((x,αs) -> sum(m(x,αs)), test_image, αs)
+grad = gradient((x,αs) -> gshow(sum(gshow(m(x,αs)))), test_image, αs)
 
 gαs = gradient(params(αs)) do
     sum(m(test_image, αs))

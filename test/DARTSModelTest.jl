2cc420d70bd3f6e34aa847997bc002fee3f69b54
k8lion <kaitlinmaile@gmail.com>
2020-12-27 22:43:30 +0100

    more testing

--- test/DARTSModelTest.jl
+++ test/DARTSModelTest.jl
@@ -27,24 +27,19 @@ end
     data = rand(Float32,8,8,4,2) |> gpu
     cell = Cell(4, 4, 1, false, false, 4, 4) |> gpu
     @test length(Flux.params(cell).order) > 0
-    #as = α14()
     as = [2e-3*(rand(num_ops).-0.5) |> f32 |> gpu  for _ in 1:k]
-    #as = rand(Float32, k, num_ops) #|> gpu
     @test size(data) == size(cell(data, data, as))
     grad = gradient((x1, x2, αs) -> sum(cell(x1, x2, αs)), data, data, as)
     @test size(data) == size(grad[1])
-    display(typeof(grad[3]))
 end
 
 @testset "DARTS Model" begin
     m = DARTSModel() |> gpu
     @test length(Flux.params(m).order) > 1
-    @test length(all_αs(m).order) == 2*k
     @test length(all_αs(m).order) + length(all_ws(m).order) == length(Flux.params(m).order)
     @test length(Flux.params(m.cells).order) > 1
     test_image = rand(Float32, 32, 32, 3, 1) |> gpu
     grad = gradient(x->sum(m(x)), test_image)
-    @show m.activations
     @test size(test_image) ==  size(grad[1])
     loss(m, x) = sum(m(x))
     gws = gradient(Flux.params(m.cells)) do
@@ -55,5 +50,34 @@ end
         sum(m(test_image))
     end
     @test typeof(gαs[Flux.params(m.normal_αs)[1]]) != Nothing
-    @show m.activations
+    @test length(m.activations.activations) > 0
+end
+
+@testset "DARTS Eval Cell" begin
+    steps = 4
+    k = floor(Int, steps^2/2+3*steps/2)
+    num_ops = length(PRIMITIVES)
+    data = rand(Float32,8,8,4,2) |> gpu
+    cell = EvalCell(4, 4, 1, false, false, 4, 4) |> gpu
+    @test length(Flux.params(cell).order) > 0
+    as = [2e-3*(rand(num_ops).-0.5) |> f32 |> gpu  for _ in 1:k]
+    @test size(data) == size(cell(data, data, as))
+    grad = gradient((x1, x2, αs) -> sum(cell(x1, x2, αs, 0.4)), data, data, as)
+    @test size(data) == size(grad[1])
+end
+
+@testset "DARTS Eval Model" begin
+    normal = [2e-3*(rand(num_ops).-0.5) |> f32 |> gpu  for _ in 1:k]
+    reduce = [2e-3*(rand(num_ops).-0.5) |> f32 |> gpu  for _ in 1:k]
+    m = DARTSEvalModel(normal, reduce) |> gpu
+    @test length(Flux.params(m).order) > 1
+    @test length(Flux.params(m.cells).order) > 1
+    test_image = rand(Float32, 32, 32, 3, 1) |> gpu
+    grad = gradient(x->sum(m(x)), test_image)
+    @test size(test_image) ==  size(grad[1])
+    loss(m, x) = sum(m(x, 0.4))
+    gws = gradient(Flux.params(m.cells)) do
+        sum(m(test_image))
+    end
+    @test typeof(gws[Flux.params(m.cells)[1]]) != Nothing
 end

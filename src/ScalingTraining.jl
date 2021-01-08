9b9517f0f6126d0cb10051ab62e2e5bee2aed880
k8lion <kaitlinmaile@gmail.com>
2021-01-08 13:23:46 +0100

    Scaling factors

---
+++ src/ScalingTraining.jl
@@ -0,0 +1,161 @@
+export Scalingtrain1st!, scalingupdate, collectscales
+
+using Flux
+using Flux: onehotbatch
+using Juno
+using Base.Iterators
+using StatsBase
+using Zygote
+using Zygote: @nograd
+using LinearAlgebra
+using CUDA
+using Plots
+include("utils.jl")
+include("DARTSModel.jl")
+
+function collectscales(model)
+    for (i,cell) in enumerate(m.cells)
+        #cell.reduction
+        for (j,mixedop) in enumerate(cell.mixedops)
+            for (k,op) in enumerate(mixedop.ops)
+                for (l,layer) in enumerate(op.op)
+                    if typeof(layer) <: Flux.BatchNorm
+                        @show (i,j,k,l,layer.λ)
+                    end
+                end
+            end
+        end
+    end
+end
+
+
+
+connectstrings = vcat([[string(j,"-",i) for j = 1:i-1] for i = 3:6]...)
+axlab = vcat([string("n",e) for e in connectstrings],[string("r",e) for e in connectstrings])
+
+edgedict = Dict{String, Int64}()
+for (i,strconn) in enumerate(connectstrings)
+    edgedict[strconn] = i
+end
+primdict = Dict{String, Int64}()
+for (i,prim) in enumerate(PRIMITIVES)
+    primdict[prim] = i
+end
+
+function scalingupdate(model)
+    reducecells = Array{Int64, 1}()
+    normalcells = Array{Int64, 1}()
+    scales = Array{Float32, 3}(undef, length(model.cells), length(model.cells[1].mixedops), length(model.cells[1].mixedops[1].ops)) |> gpu#cell id x mixop id x op id
+    for (i,cell) in enumerate(model.cells)
+        if cell.reduction
+            push!(reducecells, i)
+        else
+            push!(normalcells, i)
+        end
+        for (j,mixedop) in enumerate(cell.mixedops)
+            for (k,op) in enumerate(mixedop.ops)
+                scales[i,j,k] = 0.0
+                for (l,layer) in enumerate(op.op)
+                    if typeof(layer) <: Flux.BatchNorm
+                        #@show(k, mean(layer.γ .- 1.0))
+                        scales[i,j,k] = mean(layer.γ .- 1.0)  #automatically gets only last BatchNorm in op
+                    end
+                end
+            end
+        end
+    end
+    reduce = scales[reducecells,:,:]
+    normal = scales[normalcells,:,:]
+    display(vcat(dropdims(mean(normal,dims=1),dims=1),dropdims(mean(reduce,dims=1),dims=1)))
+    vcat(dropdims(mean(normal,dims=1),dims=1),dropdims(mean(reduce,dims=1),dims=1))'
+end
+
+function scalingreg(model)
+    reducecells = Array{Int64, 1}()
+    normalcells = Array{Int64, 1}()
+    collectscales = 0.0
+    for (i,cell) in enumerate(model.cells)
+        for (j,mixedop) in enumerate(cell.mixedops)
+            for (k,op) in enumerate(mixedop.ops)
+                for (l,layer) in enumerate(op.op)
+                    if typeof(layer) <: Flux.BatchNorm
+                        collectscales += abs(mean(layer.γ))
+                    end
+                end
+            end
+        end
+    end
+    collectscales
+end
+
+function flatten_grads(grads)
+    xs = Zygote.Buffer([])
+    fmap(grads) do x
+        x isa AbstractArray && push!(xs, x)
+        #println("x ",x)
+        return x
+    end
+    flat_gs = vcat(vec.(copy(xs))...)
+end
+
+function update_all!(opt, ps::Params, gs)
+    for (p, g) in zip(ps, gs)
+        g == nothing && continue
+        Flux.Optimise.update!(opt, p, g)
+    end
+end
+
+runall(f) = f
+runall(fs::AbstractVector) = () -> foreach(call, fs)
+
+all_αs(model::DARTSModel) = Flux.params([model.normal_αs, model.reduce_αs])
+all_ws(model::DARTSModel) = Flux.params([model.stem, model.cells..., model.global_pooling, model.classifier])
+
+i = 1
+function Scalingtrain1st!(loss, model, train, val, opt_α, opt_w, λ=0.0001; cbepoch = () -> (), cbbatch = () -> ())
+    local train_loss
+    local val_loss
+    local gsα
+    w = all_ws(model)
+    α = all_αs(model)
+    fakeg = all_αs(model).*0
+    fakea = all_αs(model).*0
+    global i
+    for (train_batch, val_batch) in zip(CuIterator(train), CuIterator(val))
+        gsw = gradient(w) do
+            train_loss = loss(model, train_batch...) + λ*scalingreg(model)#sum([sum(abs, ss) for ss in scalingupdate(model)])
+            return train_loss
+        end
+        foreach(CUDA.unsafe_free!, train_batch)
+        Flux.Optimise.update!(opt_w, w, gsw)
+        CUDA.reclaim()
+
+        gsα = gradient(α) do
+            val_loss = loss(model, val_batch...)
+            return val_loss
+        end
+        foreach(CUDA.unsafe_free!, val_batch)
+        CUDA.reclaim()
+        GC.gc()
+        gs = [gsα[x] for x in α]
+        for (f,g) in zip(fakeg, gs)
+            f .-= g
+        end
+        CUDA.reclaim()
+        GC.gc()
+        cbbatch()
+    end
+    #ac1 = activationupdatesd(model)
+    @show size(scalingupdate(model)|>cpu)
+    @show size(hcat((fakeg |> cpu)...))
+    p1 = heatmap(axlab, PRIMITIVES, scalingupdate(model)|>cpu, xrotation = 90, title = "Mean Scaling Delta")
+    #ac2 = activationupdatema(model)
+    #p2 = heatmap(axlab, PRIMITIVES, hcat((ac2 |> cpu)...), xrotation = 90, title = "Activation Mean of Absolute Value")
+    #gs = [gsα[x] for x in α]
+    p3 = heatmap(axlab, PRIMITIVES, hcat((fakeg |> cpu)...), xrotation = 90 , title = "Alpha Gradient Update")
+    plot(p1,p3,layout = (2,1),size = (1200, 800))
+    savefig(string("test/models/fig_scale", i, ".png"));
+
+    i += 1
+    cbepoch()
+end
--- test/ScalingTrainingScript.jl
+++ test/ScalingTrainingScript.jl
@@ -8,8 +8,9 @@ using CUDA
 using Distributions
 using BSON
 using Dates
+using Plots
+
 include("CIFAR10.jl")
-@nograd onehotbatch
 
 @with_kw struct trial_params
     epochs::Int = 50
@@ -20,9 +21,11 @@ include("CIFAR10.jl")
     test_fraction::Float32 = 1.0
 end
 
-argparams = trial_params(val_split = 0.1, batchsize=32, trainval_fraction = 0.01)
+argparams = trial_params(batchsize = 32, trainval_fraction = 0.01)
 
 num_ops = length(PRIMITIVES)
+
+m = DARTSModel(num_cells = 4, channels = 4) |> gpu
 
 losscb() = @show(loss(m, test[1] |> gpu, test[2] |> gpu))
 throttled_losscb = throttle(losscb, argparams.throttle_)
@@ -32,39 +35,25 @@ end
 
 acccb() = @show(accuracy_batched(m, val))
 function accuracy(m, x, y; pert = [])
-    out = mean(onecold(m(x, αs = pert), 1:10) .== onecold(y, 1:10))
+    mean(onecold(m(x, normal_αs = pert), 1:10) .== onecold(y, 1:10))
 end
 function accuracy_batched(m, xy; pert = [])
     CUDA.reclaim()
-    GC.gc()
     score = 0.0
     count = 0
     for batch in CuIterator(xy)
-        @show acc = accuracy(m, batch..., pert = pert)
+        acc = accuracy(m, batch..., pert = pert)
         score += acc*length(batch)
         count += length(batch)
         CUDA.reclaim()
-        GC.gc()
     end
-    display(score / count)
-    score / count
-end
-function accuracy_unbatched(m, xy; pert = [])
-    CUDA.reclaim()
-    GC.gc()
-    xy = xy | gpu
-    acc = accuracy(m, xy..., pert = pert)
-    foreach(CUDA.unsafe_free!, xy)
-    CUDA.reclaim()
-    GC.gc()
-    acc
+    @show score / count
 end
 
 optimizer_α = ADAM(3e-4,(0.9,0.999))
 optimizer_w = Nesterov(0.025,0.9) #change?
 
-val_batchsize = 32
-train, val = get_processed_data(argparams.val_split, argparams.batchsize, argparams.trainval_fraction, val_batchsize)
+train, val = get_processed_data(argparams.val_split, argparams.batchsize, argparams.trainval_fraction)
 test = get_test_data(argparams.test_fraction)
 
 Base.@kwdef mutable struct historiessm
@@ -78,7 +67,7 @@ function (hist::historiessm)()#accuracies = false)
     push!(hist.normal_αs_sm, softmax.(copy(m.normal_αs)) |> cpu)
     push!(hist.reduce_αs_sm, softmax.(copy(m.reduce_αs)) |> cpu)
     push!(hist.activations, copy(m.activations.currentacts) |> cpu)
-
+    #push!(hist.accuracies, accuracy_batched(m, val))
     CUDA.reclaim()
     GC.gc()
 end
@@ -86,7 +75,7 @@ histepoch = historiessm([],[],[],[])
 histbatch = historiessm([],[],[],[])
 
 datesnow = Dates.now()
-base_folder = string("test/models/admm_", datesnow)
+base_folder = string("test/models/darts_", datesnow)
 mkpath(base_folder)
 function save_progress()
     m_cpu = m |> cpu
@@ -98,18 +87,14 @@ function save_progress()
     BSON.@save joinpath(base_folder, "histbatch.bson") histbatch
 end
 
+
 struct CbAll
     cbs
 end
 CbAll(cbs...) = CbAll(cbs)
 
 (cba::CbAll)() = foreach(cb -> cb(), cba.cbs)
-cbepoch = CbAll(CUDA.reclaim, GC.gc, histepoch, save_progress, CUDA.reclaim, GC.gc)
-cbbatch = CbAll(CUDA.reclaim, GC.gc, histbatch, CUDA.reclaim, GC.gc)
+cbepoch = CbAll(CUDA.reclaim, histepoch, save_progress, CUDA.reclaim)
+cbbatch = CbAll(CUDA.reclaim, histbatch, CUDA.reclaim)
 
-
-m = DARTSModel(num_cells = 4, channels = 4)
-m = gpu(m)
-zs = 0*vcat(m.normal_αs, m.reduce_αs)
-us = 0*vcat(m.normal_αs, m.reduce_αs)
-Flux.@epochs 10 ADMMtrain1st!(loss, m, train, val, optimizer_w, optimizer_α, zs, us, 1e-3; cbepoch = cbepoch, cbbatch = cbbatch)
+Flux.@epochs 10 Scalingtrain1st!(loss, m, train, val, optimizer_α, optimizer_w, 0.1)

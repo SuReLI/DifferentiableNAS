5a41f7bb876ab7ee74f6f2803aec85109193498f
k8lion <kaitlinmaile@gmail.com>
2021-01-12 16:47:40 +0100

    get rid of metalhead

--- test/CIFAR10.jl
+++ test/CIFAR10.jl
@@ -1,24 +1,18 @@
-using Metalhead, Images, Flux
+using Images, Flux, MLDatasets
 using Flux: onehotbatch
 using Zygote: @nograd
 using Base.Iterators: partition
 using Random
 using CUDA
 
-@nograd onehotbatch
-# Function to convert the RGB image to Float64 Arrays
-function getarray(X)
-    Float32.(permutedims(channelview(X), (2, 3, 1)))
-end
-
 function get_test_data(get_proportion = 1.0, batchsize = 0)
     # Fetch the test data from Metalhead and get it into proper shape.
-    test = valimgs(CIFAR10)
-    if get_proportion < 1.0
-        test = test[shuffle(1:length(test))[1:Int64(length(test)*get_proportion)]]
-    end
-    testimgs = [getarray(test[i].img) for i in 1:length(test)]
-	testY = Matrix(onehotbatch([test[i].ground_truth.class for i in 1:length(test)], 1:10))
+    test_x, test_y = MLDatasets.CIFAR10.testdata(Float32)
+	total_img = Int(floor(length(test_y)*get_proportion))
+	order = shuffle(1:total_img)
+    test = [(test_x[:,:,:,i], test_y[i]) for i in order]
+    testimgs = [test[i][1] for i in 1:length(test)]
+	testY = Matrix(onehotbatch([test[i][2] for i in 1:length(test)], 0:9))
 	if batchsize  == 0
 	    testX = cat(testimgs..., dims = 4)
 	    test = (testX,testY)
@@ -31,10 +25,13 @@ end
 
 function get_processed_data(splitr = 0.5, batchsize = 64, mini = 1.0, val_batchsize = 0)
     # Fetching the train and validation data and getting them into proper shape
-    total_img = Int(floor(40000*mini))
-    X = shuffle(trainimgs(CIFAR10))
-    imgs = [getarray(X[i].img) for i in 1:total_img]
-    labels = Matrix(onehotbatch([X[i].ground_truth.class for i in 1:total_img],1:10))
+	train_x, train_y = MLDatasets.CIFAR10.traindata(Float32)
+    total_img = Int(floor(length(train_y)*mini))
+	order = shuffle(1:total_img)
+    X = train_x[:,:,:,order]
+	y = train_y[order]
+    imgs = [X[:,:,:,i] for i in 1:total_img]
+    labels = Matrix(onehotbatch([y[i] for i in 1:total_img],0:9))
     train_pop = Int(floor((1-splitr)* total_img))
     train = [(cat(imgs[i]..., dims = 4), labels[:,i]) for i in partition(1:train_pop, batchsize)]
     if train_pop < total_img

9024f5f3bd1be92fad07605b38ba8ef56df6d045
k8lion <kaitlinmaile@gmail.com>
2021-01-13 13:03:15 +0100

    hyperparams

--- test/scripts/ADMMTrainingScript.jl
+++ test/scripts/ADMMTrainingScript.jl
@@ -13,15 +13,15 @@ include("../training_utils.jl")
 @nograd onehotbatch
 
 
-argparams = trial_params(val_split = 0.1, batchsize=32, trainval_fraction = 0.01)
+argparams = trial_params(batchsize=32)
 
 num_ops = length(PRIMITIVES)
 
 optimizer_α = ADAM(3e-4,(0.9,0.999))
 optimizer_w = Nesterov(0.025,0.9) #change?
 
-val_batchsize = 32
-train, val = get_processed_data(argparams.val_split, argparams.batchsize, argparams.trainval_fraction, val_batchsize)
+train, val = get_processed_data(argparams.val_split, argparams.batchsize, argparams.trainval_fraction)
+@show (length(train), length(val))
 test = get_test_data(argparams.test_fraction)
 
 histepoch = historiessml()
@@ -35,8 +35,8 @@ mkpath(base_folder)
 cbepoch = CbAll(CUDA.reclaim, GC.gc, histepoch, save_progress, CUDA.reclaim, GC.gc)
 cbbatch = CbAll(CUDA.reclaim, GC.gc, histbatch, CUDA.reclaim, GC.gc)
 
-m = DARTSModel(num_cells = 4, channels = 4)
+m = DARTSModel()
 m = gpu(m)
 zs = 0*vcat(m.normal_αs, m.reduce_αs)
 us = 0*vcat(m.normal_αs, m.reduce_αs)
-Flux.@epochs 10 ADMMtrain1st!(loss, m, train, val, optimizer_w, optimizer_α, zs, us, 1e-3, losses; cbepoch = cbepoch, cbbatch = cbbatch)
+Flux.@epochs 50 ADMMtrain1st!(loss, m, train, val, optimizer_w, optimizer_α, zs, us, 1e-3, losses; cbepoch = cbepoch, cbbatch = cbbatch)

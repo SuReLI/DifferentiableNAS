858ab057d1d2ff8f314848fc2ecbc0466516b60b
k8lion <kaitlinmaile@gmail.com>
2021-01-23 20:44:44 +0100

    argparse

--- test/scripts/DARTSTrainingScript.jl
+++ test/scripts/DARTSTrainingScript.jl
@@ -12,23 +12,24 @@ using Dates
 include("../CIFAR10.jl")
 include("../training_utils.jl")
 
-argparams = trial_params()
+@show args = parse_commandline()
+#argparams = trial_params()
 
 num_ops = length(PRIMITIVES)
 
-m = DARTSModel(num_cells = argparams.num_cells, channels = argparams.channels) |> gpu
+m = DARTSModel(num_cells = args["num_cells"], channels = args["channels"]) |> gpu
 
 optimiser_α = Optimiser(WeightDecay(1e-3),ADAM(3e-4,(0.5,0.999)))
 optimiser_w = Optimiser(WeightDecay(3e-4),Momentum(0.025, 0.9))
 
-train, val = get_processed_data(argparams.val_split, argparams.batchsize, argparams.trainval_fraction)
-test = get_test_data(argparams.test_fraction)
+train, val = get_processed_data(args["val_split"], args["batchsize"], args["trainval_fraction"])
+test = get_test_data(args["test_fraction"])
 
 histepoch = historiessml()
 histbatch = historiessml()
 losses = [0.0, 0.0]
 
-base_folder = prepare_folder("darts")
+base_folder = prepare_folder("darts", args)
 
 cbepoch = CbAll(CUDA.reclaim, histepoch, save_progress, CUDA.reclaim)
 cbbatch = CbAll(CUDA.reclaim, histbatch, CUDA.reclaim)
